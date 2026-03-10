239d5e08b6057980f5a1a2407796c768adaac2cf
Hong5175 <sony51755175@gmail.com>
2026-03-10 16:44:09 +0900

    [Silver I] Title: 지름길, Time: 104 ms, Memory: 14176 KB -BaekjoonHub

--- 백준/Silver/1446. 지름길/지름길.java
+++ 백준/Silver/1446. 지름길/지름길.java
@@ -1,67 +1,50 @@
-
 import java.io.*;
 import java.util.*;
 
 public class Main {
-	static int n, D;
-    static final int INF = Integer.MAX_VALUE;
-	static List<int[]> graph[];
-	static int dist[];
-	public static void main(String[] args) throws IOException{
+	public static int N, D, minDist[], shortcut[][];
+	
+	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
-		n = Integer.parseInt(st.nextToken());
+		
+		N = Integer.parseInt(st.nextToken());
 		D = Integer.parseInt(st.nextToken());
 		
-		graph = new ArrayList[D + 1];
-		dist = new int[D + 1];
-		
-		for(int i = 0;  i < D + 1; i++) {
-			graph[i] = new ArrayList<>();
-			dist[i] = INF;
-		}
-		
-        for (int i = 0; i < D; i++) {
-            graph[i].add(new int[] {i + 1, 1});
-        }
-        
-		for(int i = 0; i < n; i++) {
+		shortcut = new int[N][3];
+		for(int i = 0; i < N; i++) {
 			st = new StringTokenizer(br.readLine());
+			
 			int start = Integer.parseInt(st.nextToken());
 			int end = Integer.parseInt(st.nextToken());
-			int dist = Integer.parseInt(st.nextToken());
+			int length = Integer.parseInt(st.nextToken());
 			
+			if(length > end - start) continue;
 			if(end > D) continue;
-			if(end - start <= dist) continue;
-			graph[start].add(new int[] {end, dist});
+			
+			shortcut[i][0] = start;
+			shortcut[i][1] = end;
+			shortcut[i][2] = length;
 		}
 		
-		dij(0);
-		System.out.println(dist[D]);
-	}
-	
-	static void dij(int start) {
-		PriorityQueue<int[]> pq = new PriorityQueue<>((a, b) -> a[1] - b[1]);
-		dist[start] = 0;
-		pq.offer(new int[] {start, 0});
-		
-		while(!pq.isEmpty()) {
-			int cur[] = pq.poll();
-			int v = cur[0];
-			int cost = cur[1];
+		minDist = new int[D+1];
+		for(int i = 0; i <= D; i++)
+			minDist[i] = i;
+
+		for(int i = 0; i <= D; i++) {
 			
-			if(cost > dist[v]) continue;
+			if(i > 0) {
+				minDist[i] = Math.min(minDist[i], minDist[i-1]+1);
+			}
 			
-            for (int[] next : graph[v]) {
-                int nextv = next[0];
-                int nextCost = next[1];
-                int newCost = nextCost + cost;
-                
-                if(dist[nextv] > newCost) {
-                	dist[nextv] = newCost;
-                	pq.add(new int[] {nextv, newCost});
-                }
-            }
+			for(int[] road : shortcut) {
+				if(road[0] == i) {
+					if(minDist[i] + road[2] < minDist[road[1]]) {
+						minDist[road[1]] = minDist[i] + road[2];
+					}
+				}
+			}
 		}
+		System.out.println(minDist[D]);
 	}
 }
